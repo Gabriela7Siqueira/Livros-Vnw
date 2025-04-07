be925f4c53ed2fd0c89c219d9a6d04802d5a331e
Gabriela7Siqueira <gabi.luana_@hotmail.com>
2025-04-07 17:21:40 -0300

    Atualização get

--- src/Paginas/LivrosDoados/LivrosDoados.jsx
+++ src/Paginas/LivrosDoados/LivrosDoados.jsx
@@ -1,19 +1,33 @@
 import s from "./LivrosDoados.module.scss";
 import protagonista from "./protagonista.png"
+import axios from "axios"
+import { useEffect , useState} from "react";
 
 export default function LivrosDoados(){
+    const [livros,setLivros]=useState([])
+    const pegarLivros = async()=> {
+        const dados = await axios.get ("https://api-livros-doados.onrender.com/livros")
+    setLivros (dados.data)
+    }
+    useEffect (()=> {
+        pegarLivros ()
+    },[])
+
     return(
         <section>
             <h2>Livros Doados</h2>
         <section className={s.livrosDoadosSection}>
-            <section className={s.containerCards}/>
-                <img src={protagonista} alt="Imagem do Livro O Protagonista" />
-                <div>
-                <h3>O protagonista</h3>
-                <p>Susane Andrade</p>
-                <p>Ficção</p>
-                </div>
+            {livros.map ((item)=>(
+            <section className={s.containerCards}> 
+            <img src={item.imagem_url} alt="" />
+            <div>
+                <h3>{item.titulo}</h3>
+                <p>{item.autor}</p>
+                <p>{item.categoria}</p>
+            </div>
             </section>
+            ))}
         </section>
+            </section>
         )
     }
